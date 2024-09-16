638121234b01a11dd8c08eab8049643908fb1a11
yukiCodesStuff <yuki.janvier@gmail.com>
2024-09-16 09:51:13 -0500

    modifying shared thread class

--- header/WebCrawler.h
+++ header/WebCrawler.h
@@ -1,11 +1,14 @@
 #pragma once
 
 #include <mutex>
+#include "SharedQueue.h"
 
 class WebCrawler {
 private:
     std::mutex _mutex;
+    SharedQueue* sq;
 public:
     WebCrawler();
+    ~WebCrawler();
     void* ThreadStartRoutine();
 };
--- objects/WebCrawler.cpp
+++ objects/WebCrawler.cpp
@@ -1,7 +1,11 @@
 #include "WebCrawler.h"
 
 WebCrawler::WebCrawler() {
+    this->sq = new SharedQueue();
+}
 
+WebCrawler::~WebCrawler() {
+    delete this->sq;
 }
 
 void* WebCrawler::ThreadStartRoutine() {
